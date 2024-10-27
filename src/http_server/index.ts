06475d4476a90452982852a35b2f2fe2064e9122
Dmitry Korzun <korzundm7@gmail.com>
2024-10-27 15:21:24 +0300

    init:create-webpack-config-files

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -2,16 +2,19 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as http from 'node:http';
 
-export const httpServer = http.createServer(function (req: http.IncomingMessage, res: http.ServerResponse) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err: any, data: any) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
+export const httpServer = http.createServer(function (
+  req: http.IncomingMessage,
+  res: http.ServerResponse
+) {
+  const __dirname = path.resolve(path.dirname(''));
+  const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+    fs.readFile(file_path, function (err: NodeJS.ErrnoException | null, data: Buffer) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+        return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
 });
